08e61fe5d203efe7e3bbbf02fcf8bbd94ea777ef
Ian Simpson <ian.g.simpson@gmail.com>
2013-06-01 21:31:31 -0600

    added validation for svn providers

--- src/main/java/com/theotherian/maven/plugins/SimpleReleaseScmValidator.java
+++ src/main/java/com/theotherian/maven/plugins/SimpleReleaseScmValidator.java
@@ -10,6 +10,7 @@ import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.scm.CommandParameter;
 import org.apache.maven.scm.CommandParameters;
+import org.apache.maven.scm.ScmException;
 import org.apache.maven.scm.ScmFileSet;
 import org.apache.maven.scm.command.info.InfoItem;
 import org.apache.maven.scm.command.info.InfoScmResult;
@@ -39,34 +40,73 @@ public class SimpleReleaseScmValidator extends AbstractMojo {
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
     Scm scm = project.getScm();
-    getLog().info("hello!");
-    getLog().info("scm was " + scm);
     ScmRepository repository = null;
     try {
 //      getLog().info("SCM type: " + scmProvider.getScmType());
       repository = manager.makeScmRepository(scm.getDeveloperConnection());
       getLog().info("Provider: " + repository.getProvider());
 
+
 //      ScmProviderRepositoryWithHost providerRepository = (ScmProviderRepositoryWithHost) repository.getProviderRepository();
 //      ScmProviderRepository providerRepository = scmProvider.makeProviderScmRepository(new File("."));
       ScmProviderRepository providerRepository = repository.getProviderRepository();
       ScmProvider provider = manager.getProviderByRepository(repository);
-      getLog().info("Provider thinks it was: " + provider.getScmType());
+//      getLog().info("Provider thinks it was: " + provider.getScmType());
 
-      ScmFileSet fileSet = new ScmFileSet(new File("."));
-      CommandParameters parameters = new CommandParameters();
-      InfoScmResult info = provider.info(providerRepository, fileSet, parameters);
-      RemoteInfoScmResult remoteInfo = provider.remoteInfo(providerRepository, fileSet, parameters);
-      provider.status(repository, fileSet);
-
-      getLog().info("Provider message: " + info.getProviderMessage());
-      for (InfoItem item : info.getInfoItems()) {
-        getLog().info("Info item: " + item.getURL());
+      if ("svn".equals(repository.getProvider())) {
+        validateSvnLocations(scm, provider, providerRepository);
       }
+
+//      ScmFileSet fileSet = new ScmFileSet(new File("."));
+//      CommandParameters parameters = new CommandParameters();
+//      InfoScmResult info = provider.info(providerRepository, fileSet, parameters);
+//      RemoteInfoScmResult remoteInfo = provider.remoteInfo(providerRepository, fileSet, parameters);
+//      provider.status(repository, fileSet);
+//
+//
+//      getLog().info("Provider message: " + info.getProviderMessage());
+//      for (InfoItem item : info.getInfoItems()) {
+//        getLog().info("Info item: " + item.getURL());
+//      }
     } catch (Exception e) {
       getLog().error(e);
+      throw new MojoFailureException("Validating SCM information failed", e);
     }
 
+  }
+
+  private void validateSvnLocations(Scm scm, ScmProvider provider, ScmProviderRepository providerRepository)
+      throws ScmException, MojoExecutionException {
+
+    getLog().info("Attempting to validate SCM information for SCM type: 'svn'...");
+
+    ScmFileSet fileSet = new ScmFileSet(new File("."));
+    CommandParameters parameters = new CommandParameters();
+    InfoScmResult info = provider.info(providerRepository, fileSet, parameters);
+    if (info.getInfoItems().size() > 1) {
+      getLog().warn("Can't validate SCM info: unexpected number of results from 'svn info' command: "
+          + info.getInfoItems().size());
+      return;
+    }
+    else if (info.getInfoItems().isEmpty()) {
+      getLog().warn("Can't validate SCM info: nothing returned from 'svn info' command.  " +
+          "Your release may be unstable or have invalid information.");
+      return;
+    }
+    String urlFromPom = scm.getDeveloperConnection().substring("scm:svn:".length());
+    String urlFromInfoCommand = info.getInfoItems().get(0).getURL();
+    getLog().info("SCM location from pom: '" + urlFromPom + "'");
+    getLog().info("SCM location from 'svn info': '" + urlFromInfoCommand + "'");
+    if (urlFromPom.equals(urlFromInfoCommand)) {
+      getLog().info("SCM locations equal; tagging will ensure the correct source captured for the release");
+    }
+    else {
+      String message = "SCM locations for workspace and pom SCM section are not the same.  " +
+          "Your build is not configured correctly for a release as tagging during release prepare will tag " +
+          "the incorrect location: expected '" + urlFromInfoCommand + "', but was '" + urlFromPom + "'";
+      getLog().error(message);
+      throw new MojoExecutionException(message);
+    }
   }
 
 }
